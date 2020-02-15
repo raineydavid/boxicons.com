830fd62d87d9144c41906db8dd7adaeee0c95da5
Atisa <40288907+atisawd@users.noreply.github.com>
2020-02-15 10:40:42 +0530

    2.0.5

--- _next/static/runtime/webpack.js
+++ _next/static/runtime/webpack.js
@@ -112,7 +112,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "5e00cc3ea0a0668e60b3";
+/******/ 	var hotCurrentHash = "b8fe6f456d642fcc7be9";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
